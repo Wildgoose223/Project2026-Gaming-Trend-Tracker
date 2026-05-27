28c9b87936b6f773218dd361d6bec5d1675d63bb
Samuel Obrien <samob2319@gmail.com>
2026-05-26 21:34:44 -0500

    Auto update dashboard

--- auto_update.bat
+++ auto_update.bat
@@ -1,15 +1,16 @@
 @echo off
-
 cd /d C:\Users\Samob\OneDrive\Desktop\Project2026
 
-python youtube_trends_to_db.py
+python scripts\youtube_trends_to_db.py
+python scripts\steam_trends_to_db.py
+python scripts\dashboard_generator.py
 
-python dashboard_generator.py
+copy dashboard\index.html index.html /Y
 
+git pull
 git add .
-
-git commit -m "Automated dashboard update"
-
-git pull origin main --rebase
-
+git commit -m "Auto update dashboard"
 git push
+
+echo Project2026 update complete.
+pause
